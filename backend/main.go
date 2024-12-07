04d72623a007b141af4a50b89d3887507fa8376e
mzguntalan <mzguntalan@gmail.com>
2024-12-07 23:59:34 +0900

    Connected backend to postgre
    - assumes psql is on your system
    - assumes a database named bridgezdb exists

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func handler(w http.ResponseWriter, r *http.Request){
@@ -11,6 +15,23 @@ func handler(w http.ResponseWriter, r *http.Request){
 }
 
 func main() {
+    dbpool, err := pgxpool.New(context.Background(), os.Getenv("DB_DSN"))
+    fmt.Println("Starting")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
+		os.Exit(1)
+	}
+	defer dbpool.Close()
+
+	var greeting string
+	err = dbpool.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(greeting)
     http.HandleFunc("/", handler)
     log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
